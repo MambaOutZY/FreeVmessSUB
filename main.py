18ad4dbeeffab4eb22834d97a21a4877702e37a7
MambaOutZY <41934953+MambaOutZY@users.noreply.github.com>
2021-08-31 09:18:58 +0800

    Update main.py

--- main.py
+++ main.py
@@ -11,17 +11,17 @@ requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
 
 
 def main():
-    resp = requests.get("https://etproxypool.ga/clash/proxies?nc=CN,c=HK,TW,JP&speed=30,100&type=vmess")
+    resp = requests.get("https://etproxypool.ga/clash/proxies?nc=CN,c=HK,TW,JP&speed=50,100&type=vmess")
     with open('./v2ray.txt', 'w', encoding="utf-8") as f:
         f.write(resp.text)
 
-    resp = requests.get("https://etproxypool.ga/clash/proxies?nc=CN,c=HK,TW,JP&speed=30,100&type=ss")
+    resp = requests.get("https://etproxypool.ga/clash/proxies?nc=CN,c=HK,TW,JP&speed=50,100&type=ss")
     with open('./ss.txt', 'w', encoding="utf-8") as f:
         f.write(resp.text)
-    resp = requests.get("https://etproxypool.ga/clash/proxies?nc=CN,c=HK,TW,JP&speed=30,100&type=ssr")
+    resp = requests.get("https://etproxypool.ga/clash/proxies?nc=CN,c=HK,TW,JP&speed=50,100&type=ssr")
     with open('./ssr.txt', 'w', encoding="utf-8") as f:
         f.write(resp.text)
-    resp = requests.get("https://etproxypool.ga/clash/proxies?nc=CN,c=HK,TW,JP&speed=30,100&type=trojan")
+    resp = requests.get("https://etproxypool.ga/clash/proxies?nc=CN,c=HK,TW,JP&speed=50,100&type=trojan")
     with open('./trojan.txt', 'w', encoding="utf-8") as f:
         f.write(resp.text)
 
